d6b7e613dd95b1ae03c793de153aa446c6f22da2
saadghonim <86053620+saadghonim@users.noreply.github.com>
2021-12-22 16:04:00 +0200

    edit css

---
+++ js/script.js
@@ -0,0 +1,15 @@
+let theName = "ahmed";
+let theGender = "female";
+let theAge = 30;
+
+
+
+theGender === "male" ? console.log("Mr") : console.log("Mrs")
+
+theAge < 20
+    ? console.log(20) 
+    : theAge > 20 && theAge < 60 
+    ? console.log("20 to 60") 
+    : theAge > 60 
+    ? console.log("largar than 60") 
+    : console.log("UnKnown")
