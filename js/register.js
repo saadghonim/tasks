ab69f5c1ec4adda27c42898597d6288572ae31d7
saadghonim <86053620+saadghonim@users.noreply.github.com>
2022-03-15 01:52:31 +0200

    edit js

--- js/register.js
+++ js/register.js
@@ -3,35 +3,33 @@ let email = document.querySelector("#email");
 let password = document.querySelector("#password");
 let btnSignUp = document.querySelector("#sign_Up");
 
-// btnSignUp.addEventListener("click", function(e){
+btnSignUp.addEventListener("click", function(e){
+  e.preventDefault();
+  if(username.value === ""){
+    username.focus();
+  }else if (email.value === ""){
+    email.focus();
+  }else if (password.value === ""){
+    password.focus();
+  }else{
+    localStorage.setItem("username", username.value);
+    localStorage.setItem("email", email.value);
+    localStorage.setItem("password", password.value);
+    setTimeout(() => {
+      window.location = "login.html";
+    });
+  }
+})
+
+// btnSignUp.addEventListener("foucs", function(e){
 //   e.preventDefault();
 //   if(username.value === "" || email.value === "" || password.value === ""){
-//     // alert("please fill data");
     
 
 
 //   }else{
-//     localStorage.setItem("username", username.value);
-//     localStorage.setItem("email", email.value);
-//     localStorage.setItem("password", password.value);
 
-//     setTimeout(() => {
-//       window.location = "login.html";
-//     });
 
 //   }
   
 // })
-
-btnSignUp.addEventListener("foucs", function(e){
-  e.preventDefault();
-  if(username.value === "" || email.value === "" || password.value === ""){
-    
-
-
-  }else{
-
-
-  }
-  
-})
